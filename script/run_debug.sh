7e895cfc7a8ea9d877319c185aa7c9cf91394def
Clarivy <zqc.sc.hn@gmail.com>
2023-03-14 04:07:04 +0000

    add: debug script

--- script/run_debug.sh
+++ script/run_debug.sh
@@ -1,6 +1,7 @@
 set PYTHONPATH=..
 python train.py\
-    --name test_1 \
-    --gpu_id 1 \
+    --name debug_1 \
+    --gpu_id 0 \
     --dataroot ./data/GNPFA/ \
     --tf_log \
+    --phase debug \

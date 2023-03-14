555dd7af83db91671e7bcb26bc7cf7078477cff7
Clarivy <zqc.sc.hn@gmail.com>
2023-03-14 04:07:11 +0000

    add: visualization utils

--- valid.py
+++ valid.py
@@ -27,5 +27,9 @@ for i, data in enumerate(dataset):
     audio, vertice, template, one_hot = data['audio'], data['vertice'], data['template'], data['one_hot']
     audio, vertice, template, one_hot = audio.cuda(), vertice.cuda(), template.cuda(), one_hot.cuda()
     generated = model.predict(audio, template, one_hot)
+    npy_name = os.path.basename(data['data_dir']) + '.npy'
+    npy_path = os.path.join(opt.results_dir, data['identity_name'])
+    os.makedirs(npy_path, exist_ok=True)
+    np.save(os.path.join(npy_path, npy_name), generated.detach().cpu().numpy())
     
     print(f'Processed {data["audio_dir"]} with identity {data["identity_name"]} ')
--- visualizer.py
+++ visualizer.py
@@ -2,15 +2,16 @@ from psbody.mesh import Mesh
 from data_loader import load_base_model
 import numpy as np
 import torch
-from zlw import align_vertices
+# from zlw import align_vertices
 
 # ceo_neutral = Mesh(filename='/data/new_disk/pangbai/FaceFormer/FaceFormer/output_neutral.obj').v[:3931]
 
-raw_template = Mesh(filename = "/data/new_disk/pangbai/FaceFormer/FaceFormer/data/000_generic_neutral_mesh.obj")
+raw_template = Mesh(filename = "/data/new_disk/new_disk/pangbai/FaceFormer/FaceFormer/data/USC_neutral.obj")
 template = raw_template.v / 100
-models = torch.tensor(load_base_model("./data/FLAME", scale=1/100)) - template
+# models = torch.tensor(load_base_model("./data/FLAME", scale=1/100)) - template
 # frames = torch.tensor(np.load("/data/new_disk/pangbai/FaceFormer/FaceFormer/demo/result/trim2.npy"))
-frames = torch.tensor(np.load("/data/new_disk/pangbai/FaceFormer/FaceFormer/vocaset/result/FaceTalk_170731_00024_TA_sentence21_condition_FaceTalk_170725_00137_TA.npy"))
+frames = np.load("/data/new_disk/new_disk/pangbai/FaceFormer/FaceFormer/data/results/Ada_OURuv_aligned/chn10.npy").squeeze()
+# frames = np.load("/data/new_disk/new_disk/pangbai/FaceFormer/FaceFormer/data/GNPFA/valid/chn10/Ada_OURuv_aligned/mesh_pred_all_vs_30fps.npy").squeeze()
 
 # _, R, T, coef = align_vertices(template, ceo_neutral, scale=True)
 # models *= coef
@@ -19,7 +20,7 @@ frames = torch.tensor(np.load("/data/new_disk/pangbai/FaceFormer/FaceFormer/voca
 for index, frame in enumerate(frames):
     print(f"Processing {index}")
     frame_mesh = Mesh(v = frame.reshape(-1, 3), f = raw_template.f)
-    frame_mesh.write_obj(f"./vis/trim2/{index:06}.obj")
+    frame_mesh.write_obj(f"./data/results/Ada_OURuv_aligned/std/{index:06}.obj")
     # result = (frame.reshape(55, 1, 1) * models).sum(0) + template
     # frame_mesh = Mesh(v = result, f = raw_template.f)
     # frame_mesh.write_obj(f"./vis/remap/{index:06}.obj")

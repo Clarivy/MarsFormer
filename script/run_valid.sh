4a06dfe79e3bf4241535812b3306c9e0ee43031a
Clarivy <zqc.sc.hn@gmail.com>
2023-04-03 03:17:35 +0000

    add: tar the result after validating

--- script/run_valid.sh
+++ script/run_valid.sh
@@ -1,6 +1,10 @@
+MODEL_NAME=scaled_nomask_lr_1e-4
+WHICH_EPOCH=latest
 set PYTHONPATH=..
 python valid.py\
-    --name test_1 \
+    --name $MODEL_NAME \
     --gpu_id 0 \
     --dataroot ./data/GNPFA/ \
-    --tf_log \
+    --how_many 2
+
+tar -cvf "${MODEL_NAME}_${WHICH_EPOCH}.tar" "data/results/${MODEL_NAME}_${WHICH_EPOCH}"

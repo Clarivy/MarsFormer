caee0743d23629acd52f1c5ce42dd7fcef6296d8
Clarivy <zqc.sc.hn@gmail.com>
2023-05-05 14:57:03 +0000

    upd: new demo generator

--- script/run_demo.sh
+++ script/run_demo.sh
@@ -1,8 +1,10 @@
+## 1 Speaker
 export PYOPENGL_PLATFORM=osmesa
 export MUJOCO_GL=osmesa
 python demo.py \
 --model_name vocaset \
---wav_path demo/wav/en-ch_bgm.wav \
+--wav_path demo/wav/chn_female_Audio2Face.wav \
+--output_file ./chn10.mp4 \
 --dataset vocaset \
 --vertice_dim 42186 \
 --feature_dim 64 \
@@ -10,7 +12,7 @@ python demo.py \
 --fps 30 \
 --train_subjects "014_blendshape 044blendshape 045blendshape 046blendshape 047blendshape 064blendshape" \
 --condition "014_blendshape" \
---model_template data/USC_neutral.obj \
---model_path checkpoints/scaled_nomask_lr_1e-4/latest_model.pth \
---render_template_path data/USC_neutral.obj \
+--model_template data/GNPFA_wild/identity/French_girl.obj \
+--model_path checkpoints/scaled_1speaker_lr_1e-4/latest_model.pth \
+--render_template_path data/GNPFA_wild/identity/French_girl.obj \
 --max_len 400 \
